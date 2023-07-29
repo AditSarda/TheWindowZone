65a5fd09407462b476416ef9714fd9475ccbfd5d
AditSarda <81905978+AditSarda@users.noreply.github.com>
2023-07-29 13:47:04 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -18,3 +18,11 @@ var whatsappb = document.getElementById("whatsappcontact")
 whatsappb.onclick = () => {
     location.href = "https://api.whatsapp.com/send?phone=918769069363";
 }
+
+var emailb = document.getElementById("emailcontact")
+
+emailb.onclick = () => {
+    location.href = action = "mailto:aditsarda4@gmail.com?Subject=Window Zone Inquiry"
+    method = "POST"
+    enctype = "text/plain";
+}
